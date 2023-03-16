d4bbd4edc78c90c20d90446df30e739d7596e3c6
sarakilany <sarakilany13@gmail.com>
2023-03-16 06:07:59 -0700

    feat: add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * Program uses printf to print
+ * with proper grammar, but the outcome is a piece of art,
+ * Author: Sara Kilany
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Users printf to output
+ * with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+
+  return (0);
+}
