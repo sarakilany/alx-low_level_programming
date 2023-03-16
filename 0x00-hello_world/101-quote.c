b53a7c02914187a49f1d6f452c93ab96b02c4de7
sarakilany <sarakilany13@gmail.com>
2023-03-16 07:37:02 -0700

    feat: add code to 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+/*
+ * Program to print
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Author: Sara Kilany
+ */
+
+#include <unistd.h>
+
+/**
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: 1 at sucess
+ */
+int main(void)
+{
+wirte(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"-1));
+
+return (1);
+}
