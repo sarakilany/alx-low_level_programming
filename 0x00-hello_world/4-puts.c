8fc9ade062067f86f3df1108299dff4eea877b3a
sarakilany <sarakilany13@gmail.com>
2023-03-16 05:09:28 -0700

    feat: add print functionality to 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+/*
+ * Program to pring "Programming is like building a multilingual puzzle" using puts
+ */
+int main(){
+  puts("Programming is like building a multilingual puzzle\n");
+  return 0;
+}
