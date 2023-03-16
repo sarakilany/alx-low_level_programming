8111aa12a794f046e1d1ab3a2f479d02f34b69cb
sarakilany <sarakilany13@gmail.com>
2023-03-16 05:30:07 -0700

    fix: format 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /*
- * This program prints "Programming is like building a multilingual puzzle" using puts.
- *
- *Author: Sara Kilany
- */
+* This program prints "Programming is like building a multilingual puzzle" using puts.
+*
+*Author: Sara Kilany
+*/
 
 #include <stdio.h>
 
-/**                                                                                                                                                               
+/**                                                                                                                                                              
 * This program prints "Programming is like building a multilingual puzzle" using puts.                                                                           
 *                                                                                                                                                                
 * @return 0 on success                                                                                                                                           
@@ -15,5 +15,5 @@ int main()
 {
   puts("\"Programming is like building a multilingual puzzle");
 
-  return 0;
+  return (0);
 }
