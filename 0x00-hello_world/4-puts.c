e15b190576f813c65417e12aca7361268e7e298d
sarakilany <sarakilany13@gmail.com>
2023-03-16 05:11:45 -0700

    fix: add include to 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 /*
  * Program to pring "Programming is like building a multilingual puzzle" using puts
  */
+#include <stdio.h>
+
 int main(){
   puts("Programming is like building a multilingual puzzle\n");
   return 0;
