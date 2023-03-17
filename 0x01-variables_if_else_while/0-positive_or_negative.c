93f769d020b17e27f76da1f35917800314ef8797
sarakilany <sarakilany13@gmail.com>
2023-03-17 05:46:57 -0700

    feat: add code to 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+/*
+ * A program to print type of randomly generated number
+ *
+ * Author: Sara Kilany
+ */
+
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - prints the type of number randomly generated
+ * 
+ * Returns: 0 on success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+        if (n < 0)
+	  printf("%d is negative\n", n);
+	else if (n > 0)
+	  printf("%d is positive\n", n);
+	else
+	  printf("%d is zero\n", n);
+	return (0);
+}
