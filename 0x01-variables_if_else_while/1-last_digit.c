765690db10bdce44d77a31cafe69af03ee434b82
sarakilany <sarakilany13@gmail.com>
2023-03-17 06:21:57 -0700

    feat: add code to 1-last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+/*
+ * A program to print the last digit in a number
+ *
+ * Author: Sara Kilany
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of a number randomly generated
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+n_str = itoa(n);
+n_len = strlen(n_str)
+lst_dig = n_str[n_len - 1]
+if (lst_dig > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, lst_dig);
+else if (lst_dig == 0)
+printf("Last digit of %d is %d and is 0\n", n, lst_dig);
+else if (lst_dig != 0 && lst_dig < 6)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lst_dig);
+return (0);
+}
