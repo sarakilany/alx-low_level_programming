b2f4bb3afc4e059cc0278655a6eef8958c3722f9
sarakilany <sarakilany13@gmail.com>
2023-03-17 06:31:58 -0700

    feat: add code to 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,8 +21,8 @@ char lst_dig;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-n_len = strlen(n_str)
-lst_dig = n_str[n_len - 1]
+n_len = strlen(itoa(n));
+lst_dig = itoa(n)[n_len - 1];
 if (atoi(lst_dig) > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, lst_dig);
 else if (atoi(lst_dig) == 0)
