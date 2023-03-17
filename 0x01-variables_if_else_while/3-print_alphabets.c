392e57610e6c4727d5ca61a8a546edad13e62ca6
sarakilany <sarakilany13@gmail.com>
2023-03-17 09:15:13 -0700

    feat: add code to 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+/*
+ * A program to print alphabets
+ *
+ * Author: Sara Kilany
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+
+char x = 'a';
+
+while (x <= 'z')
+{
+putchar(x);
+putchar(x - 32);
+putchar('\n');
+x++;
+}
+
+return (0);
+}
