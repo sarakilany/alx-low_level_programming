31f1483417e252cd631f15063c870f9c89e4de37
sarakilany <sarakilany13@gmail.com>
2023-03-21 02:30:13 -0700

    feat: add code to 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+/*
+ * A program to print the alphabet, in lowercase 10 times
+ * followed by a new line
+ *
+ * Author: Sara Kilany
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase 10 times
+ * followed by a newline 10
+ *
+ * Return: 0 on success
+ */
+void print_alphabet(void)
+{
+char i = 'a';
+while ( int j =0; j < 10; j++)
+{
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+i = 'a';
+}
+_putchar('\n');
+}
