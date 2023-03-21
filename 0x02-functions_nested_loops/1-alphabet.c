6b3f66072dfbf022c98dbef2269961e922a950d1
sarakilany <sarakilany13@gmail.com>
2023-03-21 02:15:14 -0700

    feat: add functions to 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+/*
+ * A program to print the alphabet, in lowercase
+ * followed by a new line
+ *
+ * Author: Sara Kilany
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase
+ * followed by a newline
+ *
+ * Return: 0 on success
+ */
+void print_alphabet(void)
+{
+while(char i = 'a'; i < = 'z'; i++)
+{
+_putchar(i)
+}
+}
+int main(void)
+{
+print_alpahbet();
+
+return (0);
+}
