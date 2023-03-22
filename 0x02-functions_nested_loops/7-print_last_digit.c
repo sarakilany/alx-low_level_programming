a79193d4643828159ba68eb45429544cbde49614
sarakilany <sarakilany13@gmail.com>
2023-03-21 17:49:20 -0700

    feat: add functionality to 7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+/*
+ * A program to reutrn the value of the last digit
+ *
+ *
+ * Author: Sara Kilany
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - return the value of the last digit
+ * @n: input integer
+ * Return: the last digit
+ */
+int _abs(int n)
+{
+
+int j = n;
+
+return (j % 10);
+
+}
