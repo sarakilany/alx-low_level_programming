854f0c6770b576b37a96aebf7813810656e298b6
sarakilany <sarakilany13@gmail.com>
2023-03-21 18:59:27 -0700

    feat: add functionality to 9-times_table.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,6 +16,21 @@
  */
 void jack_bauer(void)
 {
+int hour, minute;
 
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+// Print the hour
+if (hour < 10)
+_putchar('0');
+printf("%d:", hour);
 
+// Print the minute
+if (minute < 10)
+_putchar('0');
+printf("%d\n", minute);
 }
+}
+
