f6e6480c009bb7f48f8bb5877bd78d2450508484
sarakilany <sarakilany13@gmail.com>
2023-03-21 19:02:58 -0700

    feat: add functionality to 9-times_table.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, 
+ * jack_bauer - prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59
  * Return: void
  */
