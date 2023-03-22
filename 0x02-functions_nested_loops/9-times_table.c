9d81e7eba6c29329f11a4966262f3bb811421506
sarakilany <sarakilany13@gmail.com>
2023-03-21 18:50:33 -0700

    feat: add functionality to 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ int prod;
 while (i <= 9)
 {
 _putchar('0');
-i++;
+
 
 while (j <= 9)
 {
@@ -31,16 +31,17 @@ _putchar(',');
 _putchar(' ');
 
 prod = i * j;
-
+j++;
 if (prod <= 9)
 _putchar(' ');
 else
 _putchar((prod / 10) + '0');
 
 _putchar((prod % 10) + '0');
-j++;
+
 }
 _putchar('\n');
+i++;
 }
 
 }
