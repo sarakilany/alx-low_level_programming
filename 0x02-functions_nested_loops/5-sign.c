46ff7afb0b3b1d378b2146f43d155e8d584d93b2
sarakilany <sarakilany13@gmail.com>
2023-03-21 17:21:13 -0700

    feat: add functionality to 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,9 +18,18 @@ int print_sign(int n)
 
 int j = n;
 if (j > 0)
+{
+_putchar('-');
 return (1);
+}
 else if (j == 0)
+{
+_putchar('0');
 return (0);
+}
 else
+{
+_putchar('-');
 return (-1);
+}
 }
