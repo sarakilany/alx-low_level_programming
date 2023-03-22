c2b8157e7b927efed4bcf7150ec4cd84d1432f6f
sarakilany <sarakilany13@gmail.com>
2023-03-21 18:05:28 -0700

    feat: add functionality to 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+/*
+ * A program to add two input numbers
+ *
+ *
+ * Author: Sara Kilany
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adds tow input numbers
+ * @n: input integer
+ * @m: input integer
+ * Return: always 0
+ */
+int add(int n, int m)
+{
+
+int s = n + m;
+_putchar(s);
+return (0);
+
+}
