d66ba5349509b8db37550702b2c6e751afe8da79
sarakilany <sarakilany13@gmail.com>
2023-03-21 18:16:45 -0700

    feat: add functionality to 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+/*
+ * A program to print number to 98
+ *
+ *
+ * Author: Sara Kilany
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print to 98
+ * @n: input integer
+ * @m: input integer
+ * Return: sum of n+m
+ */
+void print_to_98(int n)
+{
+
+if (n <= 98)
+while (n <=98)
+{
+_putchar(n + '0');
+_putchar(',');
+_putchar(' ');
+n++;
+}
+else
+{
+while (n >= 98)
+{
+_putchar(n + '0');
+_putchar(',');
+_putchar(' ');
+ n--;
+}
+}
+
+return (0);
+
+}
