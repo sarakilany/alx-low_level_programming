29aa1a2406d637160f52f59485a98005fc9c0208
sarakilany <sarakilany13@gmail.com>
2023-03-23 02:40:56 -0700

    feat: add functionality to 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+/*
+ * A program to print the numbers, from 0 to 9,
+ * except 2 and 4 followed by a new line.
+ * Author: Sara Kilany
+ */
+ 
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4 followed by a new line.
+ *
+ * Return: no return
+ */
+void print_most_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
