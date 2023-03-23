e4a7f1c9dab61b348222cb0b7bcfb4958a038467
sarakilany <sarakilany13@gmail.com>
2023-03-23 07:14:07 -0700

    fix: rewrite 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
  * except 2 and 4 followed by a new line.
  * Author: Sara Kilany
  */
- 
+
 #include <stdio.h>
 #include "main.h"
 
@@ -18,7 +18,7 @@ void print_most_numbers(void)
 int number = '0';
 while (number <= '9')
 {
-if (number != '2' || number != '4')
+if (number != '2' && number != '4')
 {
 _putchar(number);
 }
