9c0f09f9ca62847166cdd533197586d2dbc8d481
sarakilany <sarakilany13@gmail.com>
2023-03-23 05:13:27 -0700

    feat: add functionality to 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,24 @@
  *
  * Return: 0 on success
  */
-int main() {
+int main()
+{
 for (int i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz ");
-} else if (i % 3 == 0)
+}
+else if (i % 3 == 0)
 {
 printf("Fizz ");
-} else if (i % 5 == 0)
+}
+else if (i % 5 == 0)
 {
 printf("Buzz ");
-} else {
+}
+else
+{
 printf("%d ", i);
 }
 }
