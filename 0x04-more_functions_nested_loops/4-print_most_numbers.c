4b121ae537b7a0d2a728554aa920bd1ca95e809e
sarakilany <sarakilany13@gmail.com>
2023-03-23 03:30:52 -0700

    fix: 4-print_more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -15,12 +15,12 @@
  */
 void print_most_numbers(void)
 {
-int i = 0;
-while (i < 10)
+int i = '0';
+while (i <= '9')
 {
-if (i != 2 && i != 4)
+if (i != '2' && i != '4')
 {
-_putchar(i + '0');
+_putchar(i);
 }
 i++;
 }
