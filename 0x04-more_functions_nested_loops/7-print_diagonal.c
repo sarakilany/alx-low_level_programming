272522fa6dad42b3be5a828cc85c9bfa46198526
sarakilany <sarakilany13@gmail.com>
2023-03-23 04:47:56 -0700

    fix: reassign counters 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,23 +13,25 @@
  * @n: number of times the character \ should be printed
  * Return: no return
  */
-void print_diagonal(int n) {
-int i = n;
-int j = 0;
-int m;
-if (i <= 0) {
+void print_diagonal(int n)
+{
+int i = 0;
+int j;
+if (n <= 0)
+{
 _putchar('\n');
 return;
 }
-
-while (j < i) {
-m = 0;
-while (m < j) {
+while (i < n)
+{
+j = 0;
+while (j < i)
+{
 _putchar(' ');
 j++;
 }
 _putchar('\\');
 _putchar('\n');
-m++;
+i++;
 }
 }
