6bb8bc2ea834525894396ad1bb80e3b2d4480b97
sarakilany <sarakilany13@gmail.com>
2023-03-23 02:14:46 -0700

    feat: add functionality to 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/*
+ * A program to multiply two numbers.
+ * Author: Sara Kilany
+ */
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiply two numbers.
+ * @a: input number
+ * @b: input number
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+return a * b;
+}
