3864b8d4a66bd71151820fcefc30cc0153055b43
sarakilany <sarakilany13@gmail.com>
2023-03-23 05:11:27 -0700

    feat: add functionality to 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+/*
+ * A program to print the numbers from 1 to 100, followed by a new line
+ *
+ * Author: Sara Kilany
+ */
+ 
+#include "main.h"
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ *
+ * Return: 0 on success
+ */
+int main() {
+for (int i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+} else if (i % 3 == 0)
+{
+printf("Fizz ");
+} else if (i % 5 == 0)
+{
+printf("Buzz ");
+} else {
+printf("%d ", i);
+}
+}
+printf("\n");
+return (0);
+}
