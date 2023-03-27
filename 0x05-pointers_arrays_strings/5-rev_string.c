1936ff8129bb7801210663781e3517d25189dcc7
sarakilany <sarakilany13@gmail.com>
2023-03-27 14:07:31 -0700

    feat: add functionality to 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+/*
+ *
+ *
+ * Author: Sara Kilany
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: string pointer
+ * Return: No return
+ */
+void rev_string(char *s)
+{
+int _len = 0;
+int counter = 0;
+char temp;
+while (*(s + _len) != '\0')
+_len++;
+while (counter <= _len/2)
+{
+temp = *(s + counter);
+*(s + counter) = *(s + _len);
+*(s + _len) = temp;
+_len--;
+counter++;
+}
+}
