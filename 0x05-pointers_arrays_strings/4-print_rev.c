8038ef4232b1209b2424f0ad437274af79d94d45
sarakilany <sarakilany13@gmail.com>
2023-03-27 11:28:39 -0700

    fix: add newline and decrement to 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -18,11 +18,12 @@ void print_rev(char *s)
 int _len = 0;
 while (*(s + _len) != '\0')
 _len++;
+_len--;
 while (_len > 0)
 {
-_putchar(*(s + --_len));
+_putchar(*(s + _len));
 _len--;
 }
-_putchar(*(s + --_len));
+_putchar(*(s + _len));
 _putchar('\n');
 }
