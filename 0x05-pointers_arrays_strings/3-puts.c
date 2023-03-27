05c7fcac5c5dad0d10fb06f77af26fd03099cfbb
sarakilany <sarakilany13@gmail.com>
2023-03-27 10:58:48 -0700

    feat: add functionality to 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+/*
+ * Program that prints a string, followed by a
+ *  new line, to stdout
+ *
+ * Author: Sara Kilany
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line
+ * to stdout
+ *
+ * @str: input string to print
+ * Return: No return
+ */
+void _puts(char *str)
+{
+int _len = 0;
+while (*(str + _len) != '\0')
+{
+_putchar(*(str + _len);
+}
+}
