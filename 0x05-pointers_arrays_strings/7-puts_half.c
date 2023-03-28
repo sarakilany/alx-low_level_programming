44eb5bb1e60f9b3d7b3190d3959a85c53757f560
sarakilany <sarakilany13@gmail.com>
2023-03-28 05:22:23 -0700

    feat: add functionality implementation for 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,5 @@
 /*
- *
+ * Program that prints half of a string
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,22 @@
 #include <stdio.h>
 
 /**
+ * puts_half - function that prints half of a string
+ * @str: string pointer
  *
- *
- *
- * Return:
+ * Return: No return
  */
+void puts_half(char *str)
+{
+int len = strlen(str);
+int start = len / 2;
+if (len % 2 != 0)
+{
+start = (len - 1) / 2;
+}
+for (int i = start; i < len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
