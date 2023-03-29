7b7376330e84bfddbb804d124520f02d1f96d062
sarakilany <sarakilany13@gmail.com>
2023-03-29 13:08:29 -0700

    feat: add functionality to 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,7 +9,7 @@
 
 /**
  * reverse_array - reverses content of an integer array
- * @*a: pointer to array of ints
+ * @a: pointer to array of ints
  * @n: number of array elements
  * Return: No return
  */
@@ -20,8 +20,8 @@ int temp = 0;
 while (i < n)
 {
 temp = a[i];
-a[i] = a[n-1];
-a[n-1] = temp;
+a[i] = a[n - 1];
+a[n - 1] = temp;
 i++;
 n--;
 }
