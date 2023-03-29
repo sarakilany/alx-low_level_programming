1642938d27077de3e82f577c154743895115e917
sarakilany <sarakilany13@gmail.com>
2023-03-29 13:01:29 -0700

    feat: add functionality to 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
 /*
- *
+ * Program that reverses the content of an array of integers
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,21 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- * Return:
+ * reverse_array - reverses content of an integer array
+ * @a: pointer to array of ints
+ * @n: number of array elements
+ * Return: No return
  */
+void reverse_array(int *a, int n)
+{
+int i = 0;
+int temp;
+while (i <= n)
+{
+temp = a[i];
+a[i] = a[n-1];
+a[n-1] = temp;
+i++;
+n--;
+}
+}
