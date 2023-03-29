ee232e64c67fc6e14636f164a6e71131069b92fc
sarakilany <sarakilany13@gmail.com>
2023-03-29 10:33:12 -0700

    feat: add functionality to 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,32 @@
 /*
- *
+ * Program that concatenates two strings
  *
  * Author: Sara Kilany
  */
 
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
- *
- *
- *
- * Return:
+ * _strcat - concatenates two strings
+ * @dest: destination pointer of two strings
+ * @src: first string pointer
+ * Return: the pointer to string concatenated
  */
+char *_strcat(char *dest, char *src)
+{
+char *p = dest;
+char *q = p;
+int _len = strlen(dest);
+p = p + _len;
+while(*src)
+{
+*p = *src;
+src++;
+p++;
+
+}
+*p = '\0';
+return q;
+}
