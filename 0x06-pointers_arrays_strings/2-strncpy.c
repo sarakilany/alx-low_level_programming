f6a0773baf5425241599e659415362110fab2504
sarakilany <sarakilany13@gmail.com>
2023-03-30 04:47:45 -0700

    feat: add functionality to 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 /*
- *
+ * Program that copies up to n characters of a string
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,24 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- * Return:
+ * _strncpy - copies up to n characters of a string
+ * @dest: destination pointer to a string
+ * @src: source pointer string
+ * @n: number of character to append to dest
+ * Return: Pointer string
  */
+char* _strncpy(char* dest, char* src, int n)
+{
+int i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
