55a618736eaf8f9c10fb4490214d7cc7b33d8439
sarakilany <sarakilany13@gmail.com>
2023-03-30 04:51:32 -0700

    feat: add functionality to 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,7 @@
  * @n: number of character to append to dest
  * Return: Pointer string
  */
-char* _strncpy(char* dest, char* src, int n)
+char *_strncpy(char* dest, char* src, int n)
 {
 int i = 0;
 while (i < n && src[i] != '\0')
