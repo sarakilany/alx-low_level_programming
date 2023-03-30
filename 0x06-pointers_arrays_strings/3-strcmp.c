b5b9f1842f64251f7d8430dafe0ea1bae27f6ebd
sarakilany <sarakilany13@gmail.com>
2023-03-30 05:00:00 -0700

    feat: add functionality to 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,5 @@
 /*
- *
+ * Program that compares two strings
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,21 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- * Return:
+ * _strcmp - that compares two strings
+ * @s1: first string pointer
+ * @s2: second string pointer
+ * Return: 0 if strings match
  */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+while (s1[i] == s2[i])
+{
+if (s1[i] == '\0')
+{
+return 0;
+}
+i++;
+}
+return s1[i] - s2[i];
+}
