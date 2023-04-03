fd7f098624409a03dbc6c4d316fc5ecc2254c2a8
sarakilany <sarakilany13@gmail.com>
2023-04-03 04:25:59 -0700

    feat: add functionality to 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,28 @@
+/*
+ * Program that fills memory with a constant byte.
+ *
+ * Author: Sara Kilany
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to memory area
+ * @b: the byte to fill with
+ * @n: number of memory bytes to fill
+ * Return: pointer to a string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+char *p = s;
+int i = 0;
+while (i < n && *p != '\0')
+{
+*p = b;
+p++;
+i++;
+}
+return (s);
+}
