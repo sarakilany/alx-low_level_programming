7d3ba46b31b36f6f0679cb2f63e5f4236d013f47
sarakilany <sarakilany13@gmail.com>
2023-04-06 13:59:29 -0700

    feat: add functionality to 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 /*
- *
+ * A program that returns the natural square root of a number
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,26 @@
 #include <stdio.h>
 
 /**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find the square root of
  *
- *
- *
- * Return:
+ * Return: -1 if number has no natural root, root otherwise
  */
+int _sqrt_recursion(int n)
+{
+int root;
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0 || n == 1)
+{
+return (n);
+}
+root = _sqrt_recursion(n - 1);
+if (root * root == n)
+{
+return (root);
+}
+return (-1);
+}
