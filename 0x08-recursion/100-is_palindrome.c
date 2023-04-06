ef1e1a3c8ded36f3d881bc91d3eae201a9940eee
sarakilany <sarakilany13@gmail.com>
2023-04-06 15:26:42 -0700

    feat: add functionality to 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,42 @@
 
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
+
+/**
+ * _strlen_recursion - find length of string recursively
+ * @s: pointer to a string
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * is_palindrome_helper - check if characters at start and end 
+ * are the same
+ * @s: pointer to a string
+ * @start: starting point to check from string s
+ * @end: end point to check from string s
+ * Return: 1 if characers are the same, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+if (start >= end)
+{
+return (1);
+}
+if (s[start] != s[end])
+{
+return (0);
+}
+return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
 /**
  * is_palindrome - returns 1 if a string is a palindrome
  *  and 0 if not.
@@ -17,10 +52,6 @@
  */
 int is_palindrome(char *s)
 {
-int length = strlen(s);
-if (length == 0)
-return (1);
-if (s[0] == s[length])
-return (is_palindrome(s + 1));
-return (0);
+int len = _strlen_recursion(s);
+return (is_palindrome_helper(s, 0, len - 1));
 }
