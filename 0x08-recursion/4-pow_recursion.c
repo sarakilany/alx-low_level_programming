a0f5802e2280ce281f81bebaf8273fda84f4bdc5
sarakilany <sarakilany13@gmail.com>
2023-04-06 14:18:17 -0700

    feat: add functionality to 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,5 @@
 /*
- *
+ * A program that  returns the value of x raised to the power of y
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,20 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- * Return:
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: number to rasie to a power y
+ * @y: power to rais x to
+ * Return: x raised to the power of y
  */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
