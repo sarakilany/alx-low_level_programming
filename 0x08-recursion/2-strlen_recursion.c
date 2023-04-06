9ca3b248799b6bbf01eec97aae72c31867cd5e50
sarakilany <sarakilany13@gmail.com>
2023-04-06 15:16:00 -0700

    feat: add functionality to 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,5 @@
 /*
- *
+ * A program that returns the length of a string
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,16 @@
 #include <stdio.h>
 
 /**
+ * _strlen_recursion - returns the length of a string
  *
- *
- *
- * Return:
+ * @s: pointer to a string
+ * Return: length of a string s
  */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(s + 1));
+}
