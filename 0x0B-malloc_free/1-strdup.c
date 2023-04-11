fee6fe14730a6290ad3f1ef8eb04349e6a035593
sarakilany <sarakilany13@gmail.com>
2023-04-11 12:31:32 -0700

    feat: add functionality to 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,6 +8,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 /**
  * _strdup - eturns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter.
