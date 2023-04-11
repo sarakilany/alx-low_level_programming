b48ae88957405b285a89f2fe431c1c10857ee8fc
sarakilany <sarakilany13@gmail.com>
2023-04-10 17:25:14 -0700

    feat: add functionality to 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,20 @@
 
 /**
  * main - prints program's name, followed by a new line.
+ * @argc: length of command line arguments 
  * @argv: pointer tocommand line array arguments
  * Return: 0 on success
  */
-int main(char *argv[])
+int main(int argc, char *argv[])
+{
+if (argc > 1)
+{
+printf("%s!\n", argv[1]);
+}
+else
 {
 printf("%s\n", argv[0]);
+}
+
 return (0);
 }
