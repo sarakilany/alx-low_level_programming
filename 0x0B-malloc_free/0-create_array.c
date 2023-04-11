230d70aeac834f3a1b15e0ce96b27455f4f535e9
sarakilany <sarakilany13@gmail.com>
2023-04-11 12:19:22 -0700

    fix: array delecration andinitialization

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,14 +17,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *array;
+char *array = (char *) malloc(size *sizeof(char));
 unsigned int i;
 
 if (size == 0)
 {
 return (NULL);
 }
-*array = (char *) malloc(size *sizeof(char));
+
 if (array == NULL)
 {
 return (NULL);
