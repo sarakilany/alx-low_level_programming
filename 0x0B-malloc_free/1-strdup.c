f7a3ad00bdde935ea95a4a87acd790aed8ca39ea
sarakilany <sarakilany13@gmail.com>
2023-04-11 12:29:50 -0700

    feat: add functionality to 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 /*
- *
+ * A progrm that returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
  *
  * Author: Sara Kilany
  */
@@ -8,8 +9,33 @@
 #include <stdio.h>
 
 /**
+ * _strdup - eturns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: a pointer to a string to copy
  *
- *
- *
- * Return:
+ * Return: a pointer to a copy of the string
  */
+char *_strdup(char *str)
+{
+int length = strlen(str);
+char *new_str = (char *) malloc((length + 1) * sizeof(char));
+int i = 0;
+if (str == NULL)
+{
+return (NULL);
+}
+
+
+if (new_str == NULL)
+{
+return (NULL);
+}
+
+while (i < length)
+{
+new_str[i] = str[i];
+i++;
+}
+new_str[length] = '\0';
+return (new_str);
+}
