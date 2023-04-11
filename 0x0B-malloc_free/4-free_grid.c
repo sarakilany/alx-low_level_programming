b752ecb679ed8f217babc2ee4f66b721c95b73f8
sarakilany <sarakilany13@gmail.com>
2023-04-11 13:03:52 -0700

    fix: betty code style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,5 @@
 /*
- * A program that frees a 2 dimensional grid 
+ * A program that frees a 2 dimensional grid
  *
  * Author: Sara Kilany
  */
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid 
+ * free_grid - frees a 2 dimensional grid
  * @grid: the address of the two dimensional grid
  * @height: height of the grid
  * Return: No return
