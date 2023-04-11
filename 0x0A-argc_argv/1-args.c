20eb4340ff0adf2691a3177d4aff0c96d7cfa6d2
sarakilany <sarakilany13@gmail.com>
2023-04-10 17:33:42 -0700

    feat: add functionality to 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,5 @@
 /*
- *
+ * A program that prints the number of arguments passed into it
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,14 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- * Return:
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments passed to main via commandline
+ * @argv: unused argument
+ * Return: 0 on success
  */
+int main(int argc, char *argv[])
+{
+if (!argv)
+printf("%d\n", argc - 1);
+return (0);
+}
