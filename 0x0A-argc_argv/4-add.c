732193c964ef3a9b33c264f7a767f5b2d7d298e9
sarakilany <sarakilany13@gmail.com>
2023-04-10 17:50:31 -0700

    feat: add functionality to 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,15 +1,54 @@
 /*
- *
+ * A program that adds positive numbers
  *
  * Author: Sara Kilany
  */
 
 #include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
 /**
- *
- *
- *
- * Return:
+ * main - adds positive numbers
+ * @argc: number of cli arguments
+ * @argv: pointer to array of cli arguments
+ * Return: 0 on success
  */
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+
+int sum = 0;
+int i = 1;
+while (i < argc)
+{
+int j = 0;
+while (argv[i][j] != '\0')
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+j++;
+}
+
+int num = atoi(argv[i]);
+
+if (num < 0)
+{
+printf("Error\n");
+return (1);
+}
+
+sum += num;
+i++;
+}
+
+printf("%d\n", sum);
+return (0);
+}
