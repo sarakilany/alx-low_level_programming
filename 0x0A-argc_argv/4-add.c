2f6c7f57b99853ddb5e9ff03e9dab7a39b437dca
sarakilany <sarakilany13@gmail.com>
2023-04-10 17:53:09 -0700

    feat: add functionality to 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,6 +15,10 @@
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
+int sum = 0;
+int i = 1;
+int j;
+int num;
 {
 if (argc == 1)
 {
@@ -22,11 +26,9 @@ printf("0\n");
 return (0);
 }
 
-int sum = 0;
-int i = 1;
 while (i < argc)
 {
-int j = 0;
+j = 0;
 while (argv[i][j] != '\0')
 {
 if (!isdigit(argv[i][j]))
@@ -37,7 +39,7 @@ return (1);
 j++;
 }
 
-int num = atoi(argv[i]);
+num = atoi(argv[i]);
 
 if (num < 0)
 {
