827b17dd3993cfd7f88b399a1a36fc36b661bf5b
sarakilany <sarakilany13@gmail.com>
2023-04-11 12:52:27 -0700

    feat: add functionality to 4-free_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,12 @@
 /*
- *
+ * A program that returns a pointer to a 2 dimensional array of integers.
  *
  * Author: Sara Kilany
  */
 
 #include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  *
  *
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,5 @@
 /*
- *
+ * A program that frees a 2 dimensional grid 
  *
  * Author: Sara Kilany
  */
@@ -8,8 +8,18 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- * Return:
+ * free_grid - frees a 2 dimensional grid 
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ * Return: No return
  */
+void free_grid(int **grid, int height)
+{
+int i = 0;
+while (i < height)
+{
+free(grid[i]);
+i++;
+}
+free(grid);
+}
