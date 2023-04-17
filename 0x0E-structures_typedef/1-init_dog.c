02d628f62bc5866afd7c49074a68460d25d7afd2
sarakilany <sarakilany13@gmail.com>
2023-04-17 11:21:08 -0700

    fix: betty style for 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -21,10 +21,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d != NULL)
 {
-d->name = (char*) malloc(strlen(name) + 1);
+d->name = (char *) malloc(strlen(name) + 1);
 strcpy(d->name, name);
 d->age = age;
-d->owner = (char*) malloc(strlen(owner) + 1);
+d->owner = (char *) malloc(strlen(owner) + 1);
 strcpy(d->owner, owner);
 }
 }
