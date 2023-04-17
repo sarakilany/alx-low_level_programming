7c750c172e98545ff62cb9e4d6994fc5299b48f1
sarakilany <sarakilany13@gmail.com>
2023-04-17 13:49:29 -0700

    feat: write function new_dog to 4-new_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,25 @@
 /*
- *
+ * A program that frees dogs structs
  *
  * Author: Sara Kilany
  */
 
-#include "main.h"
+#include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * free_dog - free dog structs
+ * @d: a dog struct
  *
- *
- *
- * Return:
+ * Return: No return
  */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
