b7c3163032c4c0e796a5f16cca1f639a82d0a53a
sarakilany <sarakilany13@gmail.com>
2023-04-17 13:23:03 -0700

    feat: write function print_dog to2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,6 +21,6 @@ return;
 }
 
 printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-printf("Age: %f\n", d.age != NULL ? d.age : "(nil)");
+printf("Age: %f\n", d->age != NULL ? d->age : 0);
 printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
