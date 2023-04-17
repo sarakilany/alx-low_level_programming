3432c979cc560d66aaa08c656892fb29ea58ac45
sarakilany <sarakilany13@gmail.com>
2023-04-17 11:25:36 -0700

    fix: betty style for 1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;
@@ -10,6 +17,9 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - Typedef for dog structure
+ */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
