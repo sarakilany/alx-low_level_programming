2a8541ddad0405fa2687f6dbe1b7779a4ea79622
Sara Kilany <44653974+sarakilany@users.noreply.github.com>
2023-04-19 05:14:11 +0200

    Delete 0x0D-preprocessor directory
    Deferring cohort

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef _ABS_MACRO_
-#define _ABS_MACRO_
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,4 +0,0 @@
-#ifndef _SUM_MACRO_
-#define _SUM_MACRO_
-#define SUM(x, y) ((x) + (y))
-#endif
